bf0f66de2f92b9d42c0724366318165593d9dfef
raygift <bgtickt@gmail.com>
2020-05-14 23:33:25 +0800

    fix parser rules for join action

--- parser/ast/dml.go
+++ parser/ast/dml.go
@@ -64,6 +64,12 @@ type Join struct {
 	Tp JoinType
 	// On represents join on condition.
 	On *OnCondition
+	// Using represents join using clause.
+	Using []*ColumnName
+	// NaturalJoin represents join is natural join.
+	NaturalJoin bool
+	// StraightJoin represents a straight join.
+	StraightJoin bool
 }
 
 // Accept implements Node Accept interface.
